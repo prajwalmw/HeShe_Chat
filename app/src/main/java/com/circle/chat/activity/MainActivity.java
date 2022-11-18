2518890895006f3c32aeb0dee882d45ed91a2778
Prajwal <prajwalwaingankar@gmail.com>
2022-11-19 01:31:30 +0530

    setup changes.

--- app/src/main/java/com/circle/chat/activity/MainActivity.java
+++ app/src/main/java/com/circle/chat/activity/MainActivity.java
@@ -1,8 +1,9 @@
-package com.circle.chat;
+package com.circle.chat.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.circle.chat.R;
 
 public class MainActivity extends AppCompatActivity {
 
