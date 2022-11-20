4406897500837cf6d176363a02e3ecd655927df8
Prajwal <prajwalwaingankar@gmail.com>
2022-11-20 17:41:34 +0530

    Setup user and Profile creation is done.

---
+++ app/src/main/java/com/circle/chat/activity/UserSetupScreen.java
@@ -0,0 +1,193 @@
+package com.circle.chat.activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.circle.chat.R;
+import com.circle.chat.databinding.ActivityUserSetupScreenBinding;
+import com.circle.chat.model.CategoryModel;
+import com.circle.chat.model.User;
+import com.circle.chat.utilities.SessionManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserSetupScreen extends AppCompatActivity {
+    ActivityUserSetupScreenBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    Uri selectedImage;
+    ProgressDialog dialog;
+    private String category_value;
+    private SessionManager sessionManager;
+    private List<CategoryModel> categoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserSetupScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        sessionManager = new SessionManager(this);
+
+        // changing status bar color
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Updating profile...");
+        dialog.setCancelable(false);
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        binding.imageViewIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 45);
+            }
+        });
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.nameBox.getText().toString();
+
+                if(name.isEmpty()) {
+                    binding.nameBox.setError("Please type a name");
+                    return;
+                }
+
+                dialog.show();
+                if(selectedImage != null) {
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()) {
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imageUrl = uri.toString();
+
+                                        String uid = auth.getUid();
+                                        String phone = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.nameBox.getText().toString();
+                                        String n = sessionManager.getLoggedInUsername();
+                                        if (sessionManager.getLoggedInUsername().equalsIgnoreCase("")) // Adding username who logged-in into the session manager.
+                                            sessionManager.setLoggedInUsername(name);
+
+                                        User user = new User(uid, name, phone, imageUrl);
+                                        sessionManager.setUserModel(user, "loggedIn_UserModel");
+
+                                        Intent intent = new Intent(UserSetupScreen.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } else { // here code for uploading data in firebase database...
+                    String uid = auth.getUid();
+                    String phone = auth.getCurrentUser().getPhoneNumber();
+
+                    String n = sessionManager.getLoggedInUsername();
+                    User user = new User(uid, name, phone, "No Image");
+                    sessionManager.setUserModel(user, "loggedIn_UserModel");
+                    if (sessionManager.getLoggedInUsername().equalsIgnoreCase(""))
+                        sessionManager.setLoggedInUsername(name);   // Adding username who logged-in into the session manager.
+
+
+                            database.getReference()
+                                    .child("users")
+                                    .child(uid)
+                                    .setValue(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            dialog.dismiss();
+                                            Intent intent = new Intent(UserSetupScreen.this, MainActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    });
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data != null) {
+            if(data.getData() != null) {
+                Uri uri = data.getData(); // filepath
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                long time = new Date().getTime();
+                StorageReference reference = storage.getReference().child("Profiles").child(time+"");
+
+                reference.putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String filePath = uri.toString();
+                                    HashMap<String, Object> obj = new HashMap<>();
+                                    obj.put("image", filePath);
+
+                                    database.getReference()
+                                            .child("users")
+                                            .child(FirebaseAuth.getInstance().getUid())
+                                            .updateChildren(obj).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+
+                                                }
+                                            });
+
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+                binding.imageViewIcon.setImageURI(data.getData());
+                selectedImage = data.getData();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/circle/chat/utilities/SessionManager.java
@@ -0,0 +1,84 @@
+package com.circle.chat.utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.circle.chat.model.CategoryModel;
+import com.circle.chat.model.User;
+import com.circle.chat.model.CategoryModel;
+import com.circle.chat.model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class SessionManager {
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Context context;
+    private int PRIVATE_MODE = 0;
+    private static final String PREF_NAME = "Circle";
+    private static final String CATEGORY_SELECTED = "CATEGORY_SELECTED";
+    private static final String USER_MODEL = "USER_MODEL";
+    private static final String LOGGED_IN_USERNAME = "LOGGED_IN_USERNAME";
+
+    public SessionManager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public String getLoggedInUsername() {
+        return pref.getString(LOGGED_IN_USERNAME, "");
+    }
+
+    public void setLoggedInUsername(String username) {
+        editor.putString(LOGGED_IN_USERNAME, username);
+        editor.commit();
+    }
+
+    public String getCategorySelected() {
+        return pref.getString(CATEGORY_SELECTED, "");
+    }
+
+    public void setCategorySelected(String categorySelected) {
+        editor.putString(CATEGORY_SELECTED, categorySelected);
+        editor.commit();
+    }
+
+    public User getUserModel(String key) {
+        Gson gson = new Gson();
+        String json = pref.getString(key, null);
+        Type type = new TypeToken<User>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+
+    public void setUserModel(User usermodel, String key) {
+        SharedPreferences.Editor editor = pref.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(usermodel);
+        editor.putString(key, json);
+        editor.apply();
+    }
+
+
+    public void saveArrayList(List<CategoryModel> list, String key){
+//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = pref.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString(key, json);
+        editor.apply();
+
+    }
+
+    public List<CategoryModel> getArrayList(String key){
+//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        Gson gson = new Gson();
+        String json = pref.getString(key, null);
+        Type type = new TypeToken<List<CategoryModel>>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+
+}
